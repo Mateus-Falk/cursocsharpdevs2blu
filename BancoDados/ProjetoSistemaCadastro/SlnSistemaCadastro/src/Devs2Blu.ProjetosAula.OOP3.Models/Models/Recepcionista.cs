abf12924ce9c8efbf37c0ccdda3c51e61a459b8e
Mateus Falk <mateus.efalk@gmail.com>
2022-10-04 22:08:52 -0300

    Conectando C# com MySQL

---
+++ BancoDados/ProjetoSistemaCadastro/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.OOP3.Models/Models/Recepcionista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Devs2Blu.ProjetosAula.SistemaCadastro.Models.Enum;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjetosAula.SistemaCadastro.Models.Models
+{
+    public class Recepcionista : Pessoa
+    {
+        public Int32 Id { get; set; }
+        public String Setor { get; set; }
+
+        public Recepcionista()
+        {
+            TipoPessoa = TipoPessoa.PF;
+        }
+  
+    }
+}
