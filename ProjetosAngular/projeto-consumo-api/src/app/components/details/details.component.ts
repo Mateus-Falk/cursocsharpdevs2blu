8a618af3f64e7d09fab46e3dc6ce6c731ef811c0
Mateus Falk <mateus.efalk@gmail.com>
2023-02-23 21:29:11 -0300

    Angular - Details

--- ProjetosAngular/projeto-consumo-api/src/app/components/details/details.component.ts
+++ ProjetosAngular/projeto-consumo-api/src/app/components/details/details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Pokemon } from './../../models/api/pokemon-model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
 
+  public pokemon:Pokemon = new Pokemon({});
+  constructor(public service: ApiService,
+              public route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    let paramName = this.route.snapshot.paramMap.get('name');
+    if(paramName){
+      this.service.getPokemonByName(paramName).subscribe(
+          (resp) => {
+            this.pokemon = resp;
+          }
+        );
+    }
+  }
 }
